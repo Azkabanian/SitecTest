2514f3138c8f81081eb2fb4fdd3c7b6efb286f70
ArturYusupov <YusupovArturrr@gmail.com>
2022-10-23 17:18:56 +0300

    added viewBinding, MainFragment.kt, BaseFragment.kt

---
+++ app/src/main/java/android/sitectest/presentation/fragment/listOfUsersFragment/ListOfUsersFragment.kt
@@ -0,0 +1,7 @@
+package android.sitectest.presentation.fragment.listOfUsersFragment
+
+import android.sitectest.databinding.FragmentListOfUserBinding
+import android.sitectest.presentation.fragment.baseFragment.BaseFragment
+
+class ListOfUsersFragment : BaseFragment<FragmentListOfUserBinding>(FragmentListOfUserBinding::inflate) {
+}
